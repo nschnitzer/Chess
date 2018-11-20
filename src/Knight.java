608fd070c24c5bb8b21d0a45303bd41f5d515c54
Nathan Schnitzer <120NSchnitzer@frhsd.com>
2018-11-20 12:03:03 -0500

    Added Knight Class

--- src/Knight.java
+++ src/Knight.java
@@ -9,30 +9,61 @@ import java.util.ArrayList;
 
 public class Knight extends Piece
 {
-	//Constructor
+
 	public Knight(boolean team1)
 	{
 		super(team1);
 	}
 
-	//Moves the piece
-	//Moves in an L shape
-	public void move(int r1, int c1, int r2, int c2, Piece[][] board, ArrayList<Piece> eliminated)
+	public void move(int r1, int c1, int r2, int c2, Piece[][] board, ArrayList<Piece> eliminated) throws InvalidMoveException, CheckmateException
 	{
+		if (board[r1][c1].isTeamOne() == board[r2][c2].isTeamOne())//if the new piece location is the same team as the current piece
+		{
+			throw new InvalidMoveException();
+		}
+		else if ((Math.abs(c1-c2) == 2 && Math.abs(r1-r2) == 1) || (Math.abs(c1-c2) == 1 && Math.abs(r1-r2) == 2))
+		{
+			if (board[r2][c2] == null)
+			{
+				board[r2][c2] = board[r1][c1];
+				board[r1][c1] = null;
+			}
+			else //must be enemy piece
+			{
+				if (board[r1][c2].isTeamOne() && board[r2][c2].isTeamOne() == false)//if team 1 captures team 2
+				{
+					p2Eliminated.add(board[r2][c2]);
+				}
+				else if (board[r1][c1].isTeamOne() == false && board[r2][c2].isTeamOne())//if team 2 captures team 1
+				{
+					p1Eliminated.add(board[r2][c2]);
+				}
+				else
+				{
+					throw new InvalidMoveException();
+				}
 
+				board[r2][c2] = board[r1][c1];
+				board[r1][c1] = null;
+			}
+		}
+		else
+		{
+			throw new InvalidMoveException();
+		}
 	}
-
+	
+	public String getName()
+	{
+		return "Knight";
+	}
+	
+	
 	public String toString()
 	{
 		if (super.isTeam1 == true)
 			return "1k";
 		else
 			return "2k";
-	}
-
-	//Returns the name of the piece
-	public String getName()
-	{
-		return "Knight";
 	}
 }
